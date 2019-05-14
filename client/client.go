624f365d63182326a32fbb7866ac40a9f147c114
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-05-15 00:30:50 +0530

    Testing Godoc

--- client/client.go
+++ client/client.go
@@ -1,3 +1,33 @@
+// MiniDB-Client contains methods to programmatically interacts with the [MiniDB-Server](https://github.com/ritwik310/mini-db).
+// ```
+// package main
+
+// import (
+//     "fmt"
+//     "github.com/ritwik310/mini-db/client"
+// )
+
+// func main() {
+//     // Create a client instance (mdb)
+//     mdb := client.New("tcp", "localhost:8000") // By default the MiniDB-Server listens on Port-8000
+
+//     // Communicating to the Server
+//     resp0, err := mdb.Set("myname", "Ritwik Saha", "str") // "myname" => key, "Ritwik Saha" => value, "str" => data-type
+//     resp1, err := mdb.Get("myname")
+//     resp2, err := mdb.Delete("myname")
+//     resp3, err := mdb.Get("myname")
+
+//     if err != nil {
+//         fmt.Println("Error:", err)
+//     }
+
+//     fmt.Printf("resp0: %+v\n", resp0) // resp0["status"] == 200
+//     fmt.Printf("resp1: %+v\n", resp1) // resp1["data"] == "Ritwik Saha"
+//     fmt.Printf("resp2: %+v\n", resp2) // resp2["status"] == 200
+//     fmt.Printf("resp3: %+v\n", resp3) // resp3["error"] != nil && resp3["status"] == 400
+// }
+// ```
+
 package client
 
 import (
